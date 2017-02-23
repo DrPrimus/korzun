2b41e7a1aa3e2a2b841dadbb3518e98a2f06fd80
Vova Korzun <vovakorzun96@mail.ru>
2017-02-23 10:37:58 +0300

    JSONисправленный !!!!!

--- src/main/java/Headphones/JsonDecodeDemo.java
+++ src/main/java/Headphones/JsonDecodeDemo.java
@@ -3,6 +3,9 @@ package Headphones;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.FileWriter;
+import java.io.IOException;
+
 class JsonDecodeDemo {
 
     public static void main(String[] args) throws JSONException {
@@ -11,7 +14,27 @@ class JsonDecodeDemo {
         obj.put("num", new String("Panasonik"));
         obj.put("model", new Integer(123212));
         obj.put("turnon", new Boolean(true));
-        System.out.print(obj);
+        try (FileWriter file = new FileWriter("D:\\test.json")) {
+            file.write(obj.toString());
+            file.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        /*System.out.print(obj);
+        obj.put("name", "Headphones");
+        obj.put("num", new String("Panasonik"));
+        obj.put("model", new Integer(123212));
+        obj.put("turnon", new Boolean(true));
+        StringWriter out = new StringWriter();
+        obj.write(out);
+        String jsonText = out.toString();
+        System.out.print(jsonText); */
+        Headphones pan = new Headphones();
+        pan.getColor();
+        pan.setVolume(324);
+        pan.setColor("ЧЕРНЫЙ");
+        pan.turnonoff();
+
     }
 }
 
