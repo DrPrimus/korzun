4bfadc995109dfe1617e134a66d5eb69b9433959
Vova Korzun <vovakorzun96@mail.ru>
2017-02-23 14:27:38 +0300

    reboot-1

--- src/main/java/headphones/Reboot.java
+++ src/main/java/headphones/Reboot.java
@@ -16,11 +16,9 @@ public class Reboot extends HttpServlet{
 
         super.doGet(req, resp);
     }
-
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
-
         super.doPost(req, resp);
     }
 
